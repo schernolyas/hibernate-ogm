45fb9fa30f5a4f91b5e3ce7c66076f02070e2684
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2016-06-01 16:39:54 +0300

    correct travis
    review tests
    
    revert travis

--- orientdb/src/main/java/org/hibernate/ogm/datastore/orientdb/dialect/impl/OrientDBAssociationSnapshot.java
+++ orientdb/src/main/java/org/hibernate/ogm/datastore/orientdb/dialect/impl/OrientDBAssociationSnapshot.java
@@ -9,8 +9,6 @@ package org.hibernate.ogm.datastore.orientdb.dialect.impl;
 
 import java.util.Map;
 
-import org.hibernate.ogm.datastore.orientdb.logging.impl.Log;
-import org.hibernate.ogm.datastore.orientdb.logging.impl.LoggerFactory;
 import org.hibernate.ogm.model.key.spi.RowKey;
 import org.hibernate.ogm.model.spi.AssociationSnapshot;
 import org.hibernate.ogm.model.spi.Tuple;
