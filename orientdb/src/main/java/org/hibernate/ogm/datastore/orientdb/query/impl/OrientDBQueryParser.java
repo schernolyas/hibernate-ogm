d1c0d798f882aaeb71cc9a4c30e5faffab13a0f4
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2016-06-09 15:37:38 +0300

    Revert "fix bug"
    This reverts commit b7a7c4f7c1aaa537ab5a56a61c3ac06d02f281f5.

--- orientdb/src/main/java/org/hibernate/ogm/datastore/orientdb/query/impl/OrientDBQueryParser.java
+++ orientdb/src/main/java/org/hibernate/ogm/datastore/orientdb/query/impl/OrientDBQueryParser.java
@@ -33,7 +33,7 @@ public class OrientDBQueryParser extends BaseParser<ParameterParser.Recognizer> 
 	}
 
 	public Rule Query() {
-		return Sequence( QueryParts(), EOI, push( getJournaler() ) );
+		return Sequence( QueryParts(), EOI, push( journaler ) );
 	}
 
 	@SkipNode
@@ -46,14 +46,6 @@ public class OrientDBQueryParser extends BaseParser<ParameterParser.Recognizer> 
 		return FirstOf( Quoted(), Escaped(), NamedParameter(), Other() );
 	}
 
-	public RecognizerAdapter getAdapter() {
-		return adapter;
-	}
-
-	public Recognizer getJournaler() {
-		return journaler;
-	}
-
 	@SuppressSubnodes
 	public Rule NamedParameter() {
 		StringVar name = new StringVar( "" );
@@ -62,7 +54,7 @@ public class OrientDBQueryParser extends BaseParser<ParameterParser.Recognizer> 
 				ParameterBeginDelimiter(),
 				Sequence( OneOrMore( Letter() ), ZeroOrMore( Digit() ) ),
 				name.set( match() ),
-				getAdapter().addNamedParameter( name.get(), currentIndex() ) );
+				adapter.addNamedParameter( name.get(), currentIndex() ) );
 	}
 
 	@SuppressSubnodes
