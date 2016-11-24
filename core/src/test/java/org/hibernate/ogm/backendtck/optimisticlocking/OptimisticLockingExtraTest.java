d57746e92f0356d83b2cc9e80bd84b24217d486a
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2016-11-24 08:36:18 +0300

    skip the test for OrientDB

--- core/src/test/java/org/hibernate/ogm/backendtck/optimisticlocking/OptimisticLockingExtraTest.java
+++ core/src/test/java/org/hibernate/ogm/backendtck/optimisticlocking/OptimisticLockingExtraTest.java
@@ -34,7 +34,7 @@ import org.junit.rules.ExpectedException;
  * @author Gunnar Morling
  */
 @SkipByGridDialect(
-		value = { GridDialectType.CASSANDRA, GridDialectType.INFINISPAN_REMOTE },
+		value = { GridDialectType.CASSANDRA, GridDialectType.INFINISPAN_REMOTE, GridDialectType.ORIENTDB },
 		comment = "list - bag semantics unsupported (no primary key),  OrientDB uses paradigm 'one thread, one connection, one transaction'"
 )
 public class OptimisticLockingExtraTest extends OgmTestCase {
